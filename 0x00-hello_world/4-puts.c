509a276be7d7329468d88efc4c214d4a51daac04
Oluwadarah <daraanifowose@gmail.com>
2023-04-20 17:35:05 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- *main - prints "Programming is like building a multilingual puzzle"
- *return 0.
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
