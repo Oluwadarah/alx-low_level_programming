39f16540194536b52e217f334b47ef317f4007c1
Oluwadarah <daraanifowose@gmail.com>
2023-04-27 16:48:33 -0700

    more loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
+
 {
 	int i;
 
@@ -13,27 +14,22 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-		printf(" Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-		printf(" Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-		printf(" FizzBuzz");
-		}
-		else if (i == 1)
+			printf(" FizzBuzz");
+		} else if (i == 1)
 		{
-		printf("%d", i);
-		}
-		else
+			printf("%d", i);
+		} else
 		{
-		printf(" %d", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
 
 	return (0);
 }
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,8 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
+int main(void);
+
 int mul(int a, int b);
 
 void print_numbers(void);
