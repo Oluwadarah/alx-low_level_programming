8d40d56702734b6d034cf03a2e39af14dd8d2e22
Oluwadarah <daraanifowose@gmail.com>
2023-06-08 08:16:36 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of 
+ * print_binary - prints the binary representation of
  * a number
  * @n: number to print in binary
  */
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need 
+ * flip_bits - returns the number of bits you would need
  * to flip to get from one number to another.
- * 
+ *
  * @n: first number
  * @m: second number
  *
