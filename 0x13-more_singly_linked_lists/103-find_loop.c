c1756e6173c1b3135e31be15da06e0c04e6fcdcc
Oluwadarah <daraanifowose@gmail.com>
2023-06-12 07:31:36 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -4,7 +4,7 @@
  * find_listint_loop - finds the loop in a linked list
  * @head: linked list to search for
  *
- * Return: address of the node where the loop starts, 
+ * Return: address of the node where the loop starts,
  * or NULL if there's no loop
  */
 
